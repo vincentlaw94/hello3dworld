0408f754cf6bc260194b591a6f54b53142327364
vincentlaw94 <vincentlaw94@gmail.com>
2023-03-18 14:28:41 -0700

    Set up canvas, and add box mesh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
 import styles from "./page.module.css";
-
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Box } from "./components";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>hello 3d world</h1>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+        <Box position={[1.2 * 3, 0, 0]} />
+        <OrbitControls />
+      </Canvas>
     </main>
   );
 }
