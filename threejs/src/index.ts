19587989e5fb46632ae31062dce17b9ee8065314
Vincent <vincentlaw94@gmail.com>
2024-04-06 15:57:03 -0700

    Add debugging tool

--- threejs/src/index.ts
+++ threejs/src/index.ts
@@ -1,9 +1,13 @@
 import * as THREE from "three";
+import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import GUI from "lil-gui";
 
+const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 const scene = new THREE.Scene();
 
 //object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.SphereGeometry(1, 32, 32);
 const material = new THREE.MeshBasicMaterial({
   color: 0xff0000,
   wireframe: true,
@@ -20,8 +24,10 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
+//controls
+const controls = new OrbitControls(camera, canvas);
+
 //renderer
-const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
@@ -29,14 +35,17 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
 //animation
+
+// gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+// gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
 const clock = new THREE.Clock();
 const animate = () => {
   const elapsedTime = clock.getElapsedTime();
   //update objects
 
-  camera.position.x = Math.cos(elapsedTime);
-  camera.position.y = Math.sin(elapsedTime);
-  camera.lookAt(mesh.position);
+  //   camera.position.x = Math.cos(elapsedTime);
+  //   camera.position.y = Math.sin(elapsedTime);
+  //   camera.lookAt(mesh.position);
 
   //render
   renderer.render(scene, camera);
@@ -44,3 +53,12 @@ const animate = () => {
 };
 
 animate();
+
+/**
+ * Debug
+ */
+const gui = new GUI();
+gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
+gui.add(mesh, "visible");
+gui.add(material, "wireframe");
+gui.addColor(material, "color");
